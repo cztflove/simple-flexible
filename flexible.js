a68bf1c82091097adb60eb7491ceb4772dbb621c
cztflove <502672047@qq.com>
2020-03-31 17:36:20 +0800

    Update flexible.js
    修复安卓调整字体导致布局混乱

--- flexible.js
+++ flexible.js
@@ -4,6 +4,7 @@
 //兼容UC竖屏转横屏出现的BUG
 //自定义设计稿的宽度：designWidth
 //最大宽度:maxWidth
+//修复安卓调整字体导致布局混乱
 //这段js的最后面有两个参数记得要设置，一个为设计稿实际宽度，一个为制作稿最大宽度，例如设计稿为750，最大宽度为750，则为(750,750)
 ;(function(designWidth, maxWidth) {
 	var doc = document,
@@ -12,11 +13,12 @@
 	remStyle = document.createElement("style"),
 	tid;
 
-	function refreshRem() {
+	function refreshRem(scaleFactor = 1) {
 		var width = docEl.getBoundingClientRect().width;
 		maxWidth = maxWidth || 540;
 		width>maxWidth && (width=maxWidth);
-		var rem = width * 100 / designWidth;
+        var rem = width * 100 / designWidth;
+        rem = rem  / scaleFactor;
 		remStyle.innerHTML = 'html{font-size:' + rem + 'px;}';
 	}
 
@@ -27,27 +29,47 @@
 		wrap.appendChild(remStyle);
 		doc.write(wrap.innerHTML);
 		wrap = null;
-	}
+    }
+    
+    function getFontScaleFactor(){
+        var $dom = document.getElementsByTagName('body');
+        var scaledFontSize = parseInt(window.getComputedStyle($dom[0], null).getPropertyValue('font-size'));
+        return scaledFontSize / 16;
+    }
 	//要等 wiewport 设置好后才能执行 refreshRem，不然 refreshRem 会执行2次；
 	refreshRem();
 
 	win.addEventListener("resize", function() {
-		clearTimeout(tid); //防止执行两次
-		tid = setTimeout(refreshRem, 300);
+        clearTimeout(tid); //防止执行两次
+		tid = setTimeout(function(){
+            var scaleFactor = getFontScaleFactor();
+            refreshRem(scaleFactor);
+        }, 300);
 	}, false);
 
 	win.addEventListener("pageshow", function(e) {
 		if (e.persisted) { // 浏览器后退的时候重新计算
 			clearTimeout(tid);
-			tid = setTimeout(refreshRem, 300);
+			tid = setTimeout(function(){
+                var scaleFactor = getFontScaleFactor();
+                refreshRem(scaleFactor)
+            }, 300);
 		}
 	}, false);
 
 	if (doc.readyState === "complete") {
-		doc.body.style.fontSize = "16px";
+        doc.body.style.fontSize = "16px";
+        var scaleFactor = getFontScaleFactor();
+        if(scaleFactor !== 1){
+            refreshRem(scaleFactor);
+        }
 	} else {
 		doc.addEventListener("DOMContentLoaded", function(e) {
-			doc.body.style.fontSize = "16px";
+            doc.body.style.fontSize = "16px";
+            var scaleFactor = getFontScaleFactor();
+            if(scaleFactor !== 1){
+                refreshRem(scaleFactor);
+            }
 		}, false);
 	}
 })(750, 750);
